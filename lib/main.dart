599a4202f58ed73fc8f1268dab1cf46771201c27
YanAlves18 <yanpabloalves@gmail.com>
2023-05-05 09:07:11 -0300

    feat: add dark theme and get the phone's default theme to set in the app

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.light(), // tema claro
+      darkTheme: ThemeData.dark(), // tema escuro
+      themeMode: ThemeMode.system, // modo de tema automático
       home: const LoginScreen(),
     );
   }
